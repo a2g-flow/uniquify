43e0f8d3ae74f6eff7e552b8ebab2c872aa8691c
a2g-flow <gliderinkflow@gmail.com>
2023-10-02 14:41:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "336c28913a45587a4c10af8cd620b68f")
 
     # Authorized users to use this bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "-1006515901556").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6515901556, @leotgcf").split())
 
     # Generate a user session string
     TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQG_QAsAZmfr10ktvw0PJqvWBmDMtjGJYJRkCAJ02Wak9SJ9XteFXOCVcHwDC85GKKUCLysGJlduob5N8heu9uv9tUsYpc0_tsssyfE_6pMWr7FWZ3JtgOfHebJxrY3ac4uuPJ_4AAhiLNqdq8fsxnK0523UHGl_nMwhLvkEasEkbp7OnlMIh_eE7OFtvM-B7TJFPw25nMW-_EZnSIGJxYIehJ-E4uvBlohibWqJCbQdMH8ph3T07vnCfvxCoXhl9AfgA-kLBouyjPE9YvtAX_47XPThe4Nh9aWtQpPVlSDbnRjz8Ff0-1yUkgwMTDskoa9Gt1bPkLpkCALrJqLXie5t9YtaZAAAAAGEYMR0AA")
